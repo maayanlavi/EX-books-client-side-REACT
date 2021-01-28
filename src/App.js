070f57fa8d76b1c1e16bb09292b391dd608d3031
DanaOrr <danaaloni5@gmail.com>
2021-01-28 15:39:58 +0200

    menu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import SignIn from './views/SignIn';
+import Menu from './Components/Menu/Menu';
+//import { Menu } from '@material-ui/core';
 
 function App() {
   return (
-    <SignIn/>
+
+      <>
+        <SignIn/>
+        <Menu/>
+      </>
+
+    
   );
 }
 
---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import AllBooks from "views/AllBooks.js";
+import Profile from "views/Profile.js";
+import HomePage from "views/HomePage.js";
+import MyBooks from "views/MyBooks.js";
+import MySwap from "views/MySwap.js";
+import Maps from "views/Maps.js";
+import SighIn from "views/SighIn.js";
+import WishList from "views/WishList.js";
+
