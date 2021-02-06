6f7a393785a4272f2f4ba7dc14c27471a5f9f9df
maayanlavi <maayanlavi95@gmail.com>
2021-02-06 14:25:18 +0200

    new design - profile and reviews

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import Menu from '../Components/Menu';
+import header from '../assets/css/img/ReadBooks/header.png';
+import Search from '../Components/Search';
 
 export default class Profile extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      image: "",
-      Name: "dana" || 'Your name',
-      USERNAME: "dana5" || 'Your title',
-      Email:"dana@gmail.com" || '<name>@gmail.com',
-      Country: "Israel" || 'Your country',
-      age: "24" || 'mm/dd/yyyy'
+      Name: '',
+      Email:'',
+      City: '',
+      age: ''
     }
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
@@ -28,42 +29,41 @@ export default class Profile extends React.Component {
 
   render () {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <br/>
-        <label htmlFor='ProfilePhoto'>
-        ProfilePhoto
-        </label>
-        <img src={this.state.image} alt='Profile photo' />
+      <>
+      <div style={{position:'relative'}}>
+          <img src={header} style={{width:'100%', position: 'absolute', left:'0%', right:'0%', top: '-12%', bottom:'80%'}}  alt="header"/>
+          <h2 style={{color:'white', fontFamily:'Tahoma', position:'absolute', left:'0', right:'0', textAlign:'center', top:'50px'}}>My Profile</h2>  
+          <Search></Search>
+          <form onSubmit={this.handleSubmit} style={{position:'absolute', top:'150px', fontSize:'20px'}}>
         <br/>
         <label htmlFor='Name'>
         Name
         </label>
         <input type='text' value={this.state.Name} onChange={this.handleChange} name='Name' />
         <br/>
-        <label htmlFor='USERNAME'>
-        USERNAME
-        </label>
-        <input type='text' value={this.state.USERNAME} onChange={this.handleChange} name='USERNAME' />
-        <br/>
         <label htmlFor='Email'>
         Email
         </label>
         <input type='text' value={this.state.Email} onChange={this.handleChange} name='Email' />
         <br/>
-        <label htmlFor='Country'>
-        Country
+        <label htmlFor='City'>
+        City
         </label>
-        <input type='text' value={this.state.Country} onChange={this.handleChange} name='Country' />
+        <input type='text' value={this.state.City} onChange={this.handleChange} name='City' />
         <br/>
         <label htmlFor='age'>
         Age
         </label>
         <input type='text' value={this.state.age} onChange={this.handleChange} name='age' />
         <br/>
-        <button className='cta-primary' type='submit'>
+        <button className='cta-primary' type='submit' style={{backgroundColor:'rgba(186, 251, 103, 1)', color:'black', fontFamily:'tahoma', fontSize:'20px'}}>
         Save
         </button>
       </form>
+          
+      </div>
+      <Menu/>
+  </>
     )
   }
 }
