b27d39c4a1a79ec5c12d59df58ddccfd162dc2ce
Moriel Turjeman <moriel99@pop-os.localdomain>
2021-02-22 22:22:06 +0200

    book user management, swaps starting to work

--- src/Components/swap.js
+++ src/Components/swap.js
@@ -14,44 +14,37 @@ const useStyles = makeStyles((theme) => ({
     },
   }));
 
-class AllBooks extends Component {
-    constructor(props) {
-        super(props);
+export default function(props) {
 
-    }
-
-    style1 = {
+    const style1 = {
         textAlign: 'center',
         fontSize: '15px',
         color:'black',
         fontFamily: 'verdana',
-        position:'absolute',
+        position:'relative',
         top:'100px',
         left:'0',
         right:'0',
         border: '1px solid'
     }
 
+    console.log(props.date);
+
+    return(
+        <>
+        <div style={style1}>
+            <ButtonGroup color="black" aria-label="outlined primary button group">            
+                <Button><b>Confirm Swap</b></Button>
+                <Button>Cancel Swap</Button>
+            </ButtonGroup>
+            <p>Date: { new Date(props.date).toDateString() }</p>
+            <p>The partner's Email: {props.email}</p>
+            <p>The book you gave:  </p>
+            <p>The book you received:</p>
+            <p>Status:</p>
+        </div>
+
+        </>
+    )
     
-
-    render() {
-        return(
-            <>
-            <div style={this.style1}>
-                <ButtonGroup color="black" aria-label="outlined primary button group">            
-                    <Button><b>Confirm Swap</b></Button>
-                    <Button>Cancel Swap</Button>
-                </ButtonGroup>
-                <p>Date:</p>
-                <p>The partner's Email:</p>
-                <p>The book you gave:</p>
-                <p>The book you received:</p>
-                <p>Status:</p>
-            </div>
-
-            </>
-        )
-    }
 }
-
-export default AllBooks;
--- src/views/Book.js
+++ src/views/Book.js
@@ -4,30 +4,43 @@ import BooksButtons from '../Components/bookButtons';
 import Menu from '../Components/Menu';
 import { useParams } from "react-router-dom";
 import axios from 'axios';
-import { Chip, Grid, MenuItem, Select, Typography } from '@material-ui/core';
+import { Button, Chip, Grid, MenuItem, Select, Typography } from '@material-ui/core';
 
 
 export default function (props) {
     const [book, setBook] = useState({})
     const [bookCities, setBookCities] = useState([])
+    const [showBookWarning, setBookWarning] = useState(false);
+    const [userInfo, setUserInfo] = useState({})
+    const [selectedUser, setSelectedUser] = useState();
     const { id } = useParams()
 
+    const blockedSubjects = ["bible", "aversion therapy", "unintended consequences", "Beethoven's Fifth Symphony", "gang rape", "psychiatric hospitals", "maturity", "metanoia", "assault", "joyriding", "rape", "metafiction", "orgasm", "novella", "antisocial personality disorder", "classical music", "black comedy", "argot", "Nadsat", "dystopias", "satire", "literature", "Juvenile delinquents", "Science fiction", "Rehabilitation", "Fiction", "Teenage boys", "Criminals", "Sátira", "Muchachos adolescentes", "Criticism and interpretation", "Criminales", "English Authors", "Open Library Staff Picks", "Violence", "Slang language", "Gangs", "Ciencia-ficción", "Social conditions", "Ficción", "Behavior modification", "Long Now Manual for Civilization", "Juvenile delinquency", "Brainwashing", "Fiction, satire", "Fiction, dystopian", "Fiction, science fiction, general", "British and irish fiction (fictional works by one author)", "British and irish drama (dramatic works by one author)", "Kubrick, stanley, 1928-1999", "Burgess, anthony, 1917-1993", "Clockwork orange (Motion picture)", "Romance Ingles", "English literature", "Large type books"]
 
     useEffect(async () => {
         const getBookData = async () => {
-            try {
-                const bookInfo = await axios(`https://openlibrary.org/works/${id}.json`);
-                setBook(bookInfo.data);
-            }
-            catch (error) {
-             console.log(error);
-            }
+            const bookInfo = await axios(`https://openlibrary.org/works/${id}.json`).then(res => res.data);
+            const userInfo = await axios({
+                url: `${process.env.REACT_APP_SERVER}/api/current_user`,
+                withCredentials: true,
+            }).then(res => res.data)
+
+            setUserInfo(userInfo);
+            
+            if (bookInfo.subjects && bookInfo.subjects.some(s => blockedSubjects.includes(s) ))
+                setBookWarning(true);
+
+            console.log(bookInfo.subjects);
+
+
+            setBook(bookInfo);
+
         }
         getBookData();
-        }, [id])
+    }, [id])
 
 
-    useEffect(async() => {
+    useEffect(async () => {
         const getBookCities = async () => {
             try {
                 const cityList = await axios({
@@ -35,15 +48,42 @@ export default function (props) {
                     withCredentials: true
                 })
                 setBookCities(cityList.data);
-           }
-            catch (err) { console.log(err)}
+            }
+            catch (err) { console.log(err) }
         }
         getBookCities();
     }, [])
+
+    const createSwapRequest = () => {
+        axios({
+          url: `${process.env.REACT_APP_SERVER}/api/current_user`,
+          withCredentials: true
+        })
+        .then(res => res.data)
+        .then(userinfo => {
+            let swapRequest = {
+                user_id1: userinfo._id,
+                user_id2:selectedUser,
+                book_id1: id,
+                swap_status: 'Pending' }
+            
+            return axios({
+                withCredentials: true,
+                method: "POST",
+                data: swapRequest,
+                url: `${process.env.REACT_APP_SERVER}/api/swaps/`
+            })
+        })
+        .then(res => console.log(res));
+    }
+
     
+
     return (
         <>
-                <Grid container direction="column" className="books" style={{ position: 'absolute', top: '150px' }}>
+            <Grid container direction="column" className="books" style={{ position: 'absolute', top: '150px' }} alignItems="center" alignContent="center">
+                {  showBookWarning &&  <Typography>This book is not suited for yound audiences</Typography> } 
+                { ((showBookWarning && userInfo.age > 18) || (!showBookWarning)) &&  <>
                         <Book name={book.title} cover={book.covers ? book.covers[0] : null} subject={book.subjects} ></Book>
                         <Grid container direction="row" spacing={1} alignContent="center" justify="center">
                             {book.subjects ? book.subjects.slice(0, 5).map(s => <Grid item key={s}> <Chip size="small" label={s}></Chip> </Grid>) : <Chip size="small" label="General" ></Chip>}
@@ -51,14 +91,19 @@ export default function (props) {
                         <div className="buttons">
                             <BooksButtons id={id} />
                         </div>
-                            <Grid container direction="row" alignContent="center" alignContent="center">
+                            <Grid container direction="row" alignContent="center" justify="center">
                             <Typography>Loan from</Typography>
-                            <Select style={{marginLeft: "20px"}}>
-                                { bookCities.map(item => <MenuItem value={item._id}>{item.first_name},{item.age}-{item.city}</MenuItem>) }
-
+                            <Select style={{marginLeft: "20px"}} onChange={e => setSelectedUser(e.target.value)} >
+                                { bookCities.map(item => <MenuItem key={item._id} value={item._id}>{item.first_name},{item.age}-{item.city}</MenuItem>) }
                             </Select>
+                            <Button onClick={createSwapRequest} variant="contained" style={{marginLeft: "30px"}} disabled={selectedUser == null}>Send</Button>
                             </Grid>
-                </Grid>
+                            
+
+                            
+                            
+                  </>}
+            </Grid>
             <Menu />
         </>
     )
